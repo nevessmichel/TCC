e6fc74f1ffa482b18665d5a571ac868aa30f73c8
Michel Neves <nevess.michel@gmail.com>
2022-01-08 07:31:53 -0800

    added EMA logic and a small optimization

--- Models/ema.py
+++ Models/ema.py
@@ -1,6 +1,26 @@
 from Models.Base import Base
 
 class EMA(Base):
+    def __init__(self, smoothing = 2):
+        self.__smoothing = smoothing
+        self.__firstDone = False
+        self.__yesterday = 0
+        self.__oldSize = 0
+
+    def setSmoothing(self,smoothing):
+        self.__smoothing = smoothing
 
     def predict(self, vector, size):
-        return None
+        
+        multiplier = self.__smoothing/(1+size)
+        if(not self.__firstDone or self.__oldSize != size):
+            yesterday = 0
+            for i in range(size):
+                yesterday = (vector[i] * multiplier) + (yesterday * (1-multiplier))
+            self.__yesterday = yesterday
+            self.__firstDone = True
+            self.__oldSize = size
+        else:
+            self.__yesterday = (vector[-1] * multiplier) + (self.__yesterday * (1-multiplier))
+        
+        return self.__yesterday
